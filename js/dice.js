f63ac51ebc13ec9a136aaefce7de9fe90094f862
David Haskins <davidhaskins@gmail.com>
2013-09-01 15:44:11 -0500

    enabled ability to change qty of dice

--- js/dice.js
+++ js/dice.js
@@ -20,8 +20,8 @@ defend_die[6] = '[blank]';
 defend_die[7] = '[blank]';
 
 $(function(){
-
-$('.die').click(function(){
+$(document).on('click','.die',function(){
+//$('.die').click(function(){
     
     //console.log(this);
     $(this).html('');
@@ -81,4 +81,30 @@ $(document).ready(function(){
     $('.die').html('[unrolled]');
     $('.die').fadeTo(200,0.5);    
 });
+
+$('#number_of_dice').change(function(){
+	//var qty_of_dice = $('#number_of_dice').size();
+	var adjusted_qty_of_dice = $('#number_of_dice').val();
+	var current_qty_of_dice = $('.die').size();
+
+	//delete all existing dice
+	$('.die').remove();
+	$('.die_check_box').remove();
+	$('.die_txt_modifier').remove();
+
+	//create enough dice
+	for(var i=1;i<=adjusted_qty_of_dice;i++){
+		$('.offense_txt_modifier').append('<div class="text_box die_txt_modifier" id="text_box_'+ i + '"><input type="text"></div>');
+		$('.offense_dice').append('<div class="die offense" id="die_'+ i +'"></div>');
+		$('.offense_checkbox').append('<div class="check_box die_check_box" id="check_box_'+ i+'"><input type="checkbox"></div>');
+		$('.defense_txt_modifier').append('<div class="text_box die_txt_modifier" id="text_box_'+ (i + adjusted_qty_of_dice) + '"><input type="text"></div>');
+		$('.defense_dice').append('<div class="die defense" id="die_'+ (i + adjusted_qty_of_dice) +'"></div>');
+		$('.defense_checkbox').append('<div class="check_box die_check_box" id="check_box_'+ (i + adjusted_qty_of_dice) +'"><input type="checkbox"></div>');
+	}
+    $('.die').html('[unrolled]');
+    $('.die').fadeTo(200,0.5);    
+});
+
+
+
 });
